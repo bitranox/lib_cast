ddd984fd78942d7e775fd6df19a8550553ca24a3
bitranox <rnowotny1966@gmail.com>
2019-08-22 11:25:07 +0200

    refractor

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import subprocess
 
 
 def install(package):
-    subprocess.call([sys.executable, "-m", "pip", "install", "--upgrade", package])
+    subprocess.call([sys.executable, "-m", "pip", "install", package])
 
 
 try:
