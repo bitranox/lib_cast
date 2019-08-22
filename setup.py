a6e6841812c83c560b360bdac97576451f5ce925
bitranox <rnowotny1966@gmail.com>
2019-08-22 12:28:53 +0200

    refractor

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     url='https://github.com/bitranox/lib_cast',
     packages=['lib_cast'],
     install_requires=['pytest', 'typing', 'lib_regexp'],
-    dependency_links=['https://github.com/bitranox/lib_regexp/tarball/master'],
+    dependency_links=['git+https://github.com/bitranox/lib_regexp.git'],
     classifiers=CLASSIFIERS,
     setup_requires=['pytest-runner'],
     tests_require=['pytest'])
