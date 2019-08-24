d12ea01a4295182d5146d55a1b56920b726307fa
bitranox <rnowotny1966@gmail.com>
2019-08-24 18:20:03 +0200

    refractor

--- setup.py
+++ setup.py
@@ -6,7 +6,17 @@ import subprocess
 
 
 def install_requirements_when_using_setup_py():
-    subprocess.call([sys.executable, "-m", "pip", "install", '-r', './requirements.txt'])
+    proc = subprocess.Popen([sys.executable, "-m", "pip", "install", '-r', './requirements_setup.txt'],
+                            stdin=subprocess.PIPE,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    encoding = sys.getdefaultencoding()
+    print(stdout.decode(encoding))
+    print(stderr.decode(encoding))
+
+    if proc.returncode != 0:
+        raise RuntimeError('Error installing requirements_setup.txt')
 
 
 try:
@@ -40,17 +50,19 @@ if os.path.exists(readme_filename):
 
 install_requirements_when_using_setup_py()
 
-setup(
-    name='lib_cast',
-    version='0.0.1',
-    description=description,
-    long_description=long_description,
-    long_description_content_type='text/x-rst',
-    author='Robert Nowotny',
-    author_email='rnowotny1966@gmail.com',
-    url='https://github.com/bitranox/lib_cast',
-    packages=['lib_cast'],
-    install_requires=['pytest', 'typing', 'lib_regexp @ git+https://github.com/bitranox/lib_regexp.git'],
-    classifiers=CLASSIFIERS,
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest'])
+setup(name='lib_cast',
+      version='0.0.1',
+      description=description,
+      long_description=long_description,
+      long_description_content_type='text/x-rst',
+      author='Robert Nowotny',
+      author_email='rnowotny1966@gmail.com',
+      url='https://github.com/bitranox/lib_cast',
+      packages=['lib_cast'],
+      classifiers=CLASSIFIERS,
+      # specify what a project minimally needs to run correctly
+      install_requires=['typing', 'lib_regexp @ git+https://github.com/bitranox/lib_regexp.git'],
+      # minimally needs to run the setup script, dependencies needs also to put here for setup.py install test
+      setup_requires=['pytest-runner', 'lib_regexp @ git+https://github.com/bitranox/lib_regexp.git'],
+      # minimally needs to run tests
+      tests_require=['pytest'])
