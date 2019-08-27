80029fd7d751087c58775e6419fb961d8eafa7e1
bitranox <rnowotny1966@gmail.com>
2019-08-27 20:26:17 +0200

    refractor

--- lib_cast/__init__.py
+++ lib_cast/__init__.py
@@ -1,7 +1,13 @@
-# -*- coding: utf-8 -*-
-
+import pathlib
 from .lib_cast import *
 
+
+def get_version() -> str:
+    with open(pathlib.Path(__file__).parent / 'version.txt', mode='r') as version_file:
+        version = version_file.readline()
+    return version
+
+
 __title__ = 'lib_cast'
-__version__ = '0.0.1'
+__version__ = get_version()
 __name__ = 'lib_cast'
